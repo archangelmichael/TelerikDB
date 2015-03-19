9ee2ae5cf8433251ffaafdb61cf8be30ef3a9d68
Archangel <radishikerov@yahoo.com>
2015-03-19 14:23:52 +0200

    Additional Files

---
+++ EntityFrameworkWithMySql/CodeFirst/CodeFirst.Data/OrdersDbContext.cs
@@ -0,0 +1,19 @@
+﻿using CodeFirst.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodeFirst.Data
+{
+    public class OrdersDbContext : DbContext
+    {
+        public OrdersDbContext() : base("OrdersConnectionString")
+        {
+        }
+
+        public IDbSet<Order> Orders { get; set; }
+    }
+}
